f7f74288e70bb8c0b498f8f21b542718bf9b1686
QuangBinh <daiphapsu9@github.com>
2012-10-26 00:35:47 +0700

    update Company.java

--- DevFortress/src/devfortress/model/Company.java
+++ DevFortress/src/devfortress/model/Company.java
@@ -4,6 +4,8 @@
  */
 package devfortress.model;
 
+import java.util.List;
+
 /**
  *
  * @author Sherlock
@@ -11,6 +13,7 @@ package devfortress.model;
 public class Company {
 
     private int money;
+    private List<Employee> employeeList;
 
     public Company() {
         money =1000;
@@ -30,6 +33,14 @@ public class Company {
 
     public void decreaseMoney(int amount) {
         money -= amount;
+    }
+    
+    public void addEmployee(Employee newEmp){
+        employeeList.add(newEmp);
+    }
+    
+    public void removeEmployee(Employee emp){
+        employeeList.remove(emp);
     }
     
     public void test(){
