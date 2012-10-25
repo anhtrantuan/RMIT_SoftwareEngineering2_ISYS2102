eabe591f60bfd50085b2f507ec36b5cb2ad0c022
QuangBinh <daiphapsu9@github.com>
2012-10-25 16:18:37 +0700

    add Player.java

---
+++ DevFortress/src/devfortress/model/Player.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package devfortress.model;
+
+/**
+ *
+ * @author Sherlock
+ */
+public class Player {
+
+    private int money;
+
+    public Player() {
+    }
+
+    public Player(int money) {
+        this.money = money;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void increaseMoney(int amount) {
+        money += amount;
+    }
+
+    public void decreaseMoney(int amount) {
+        money -= amount;
+    }
+}
