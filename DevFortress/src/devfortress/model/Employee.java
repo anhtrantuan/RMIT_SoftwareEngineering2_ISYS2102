8000e036322f663b89b91058a23a351bd53f5bae
cathoanghuy <cat.hoanghuy@gmail.com>
2012-10-25 22:17:08 +0700

    add skillLevelUp

--- DevFortress/src/devfortress/model/Employee.java
+++ DevFortress/src/devfortress/model/Employee.java
@@ -4,6 +4,7 @@
  */
 package devfortress.model;
 
+import devfortress.components.constant.Skills;
 import java.util.Map;
 
 /**
@@ -11,6 +12,7 @@ import java.util.Map;
  * @author cathoanghuy
  */
 public class Employee {
+
     private String name;
     private float salary;
     private Map skillList;
@@ -21,10 +23,19 @@ public class Employee {
         this.skillList = skillList;
     }
 
-    public Employee(String name,Map skillList) {
+    public Employee(String name, Map skillList) {
         this.name = name;
         this.skillList = skillList;
     }
-    
-    
+
+    public void skillLevelUp(Skills sk) {
+        if (skillList.containsKey(sk)) {
+            for (Object object : skillList.keySet()) {
+                if (object == sk) {
+                    int val = ((Integer)skillList.get(sk)).intValue();
+                    skillList.put(sk, val++);
+                }
+            }
+        }
+    }
 }
