1941efa0e064780e66eba158b27b09ff57de8df3
QuangBinh <daiphapsu9@github.com>
2012-10-26 09:36:11 +0700

    update Engine

--- DevFortress/src/devfortress/model/facade/Engine.java
+++ DevFortress/src/devfortress/model/facade/Engine.java
@@ -4,10 +4,49 @@
  */
 package devfortress.model.facade;
 
+import devfortress.model.Employee;
+import devfortress.model.Item;
+import devfortress.model.Project;
+
 /**
  *
  * @author cathoanghuy
  */
 public class Engine implements Model{
+
+    @Override
+    public void buyFood(Item item) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void hireEmployee(Employee employee) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void fireEmployee(Employee employee) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void takeProject(Project project) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void cancelProject(Project project) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void eventOccure() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void levelUp(Project project) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
     
 }
