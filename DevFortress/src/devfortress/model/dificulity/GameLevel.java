345cbf523a72b5c3779a8051b5326ffc9afaba94
Cat Hoang Huy <cat.hoanghuy@gmail.com>
2012-10-30 00:59:26 +0700

    Add Game Level

---
+++ DevFortress/src/devfortress/model/dificulity/GameLevel.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package devfortress.model.dificulity;
+
+import devfortress.utilities.Skills;
+import java.util.Map;
+
+/**
+ *
+ * @author cathoanghuy
+ */
+public interface GameLevel {
+
+    Map<Skills, Integer> generateSkillLisl(int skillNum);
+}
---
+++ DevFortress/src/devfortress/model/dificulity/MediumLevel.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package devfortress.model.dificulity;
+
+import devfortress.utilities.Skills;
+import java.util.Map;
+
+/**
+ *
+ * @author cathoanghuy
+ */
+public class MediumLevel implements GameLevel{
+
+    @Override
+    public Map<Skills, Integer> generateSkillLisl(int skillNum) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
