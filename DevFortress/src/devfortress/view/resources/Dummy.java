2400dbe2c76a097cb120d1cc6b3638d8871034f4
anhtrantuan <anhtrantuan.hcmc@gmail.com>
2012-11-12 19:25:38 +0700

    View - Create new package

---
+++ DevFortress/src/devfortress/view/resources/Dummy.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package devfortress.view.resources;
+
+/**
+ *
+ * @author tommy
+ */
+public class Dummy {
+    
+}
