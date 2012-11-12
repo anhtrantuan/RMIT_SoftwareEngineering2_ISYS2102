09e083c82538db8941ff761975de672e6f312141
Cat Hoang Huy <cat.hoanghuy@gmail.com>
2012-11-12 15:37:17 +0700

    update Employee

--- DevFortress/test/devfortress/utilities/Test.java
+++ DevFortress/test/devfortress/utilities/Test.java
@@ -4,6 +4,7 @@
  */
 package devfortress.utilities;
 
+import devfortress.model.Employee;
 import devfortress.model.dificulity.EasyLevel;
 import devfortress.model.dificulity.GameLevel;
 import java.util.List;
@@ -13,13 +14,21 @@ import java.util.List;
  * @author cathoanghuy
  */
 public class Test {
+
     public static void main(String[] args) {
         GameLevel level = new EasyLevel();
-        int numberOfProject = 4;
-        List result = Utilities.generateProjectList(level, numberOfProject);
-        for (int i = 0; i < result.size(); i++) {
-            System.out.println(result.get(i));
+//        int numberOfProject = 4;
+//        List result = Utilities.generateProjectList(level, numberOfProject);
+//        for (int i = 0; i < result.size(); i++) {
+//            System.out.println(result.get(i));
+//        }
+
+        Employee e = new Employee("Cat Hoang Huy", level.generateSkillList());
+        for (Object skill : e.getSkillList().keySet()) {
+            System.out.println(skill.toString()+":"+e.getSkillList().get(skill));
         }
+        System.out.println(e.getLowestSkillLvl());
         
+
     }
 }
