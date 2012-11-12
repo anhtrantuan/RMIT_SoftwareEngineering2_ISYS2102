e70e44d527a07b749d2375baa5e3db69b08a23e7
QuangBinh <daiphapsu9@github.com>
2012-11-12 16:08:26 +0700

    update

---
+++ DevFortress/src/devfortress/model/exception/ProjectFailsException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package devfortress.model.exception;
+
+/**
+ *
+ * @author Sherlock
+ */
+public class ProjectFailsException extends Exception{
+
+    public ProjectFailsException(String message) {
+        super(message);
+    }
+
+    public ProjectFailsException() {
+    }
+    
+}
