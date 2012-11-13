56b2cfab7eb9aa0129ddb3e5649410aa3b72ad01
Cat Hoang Huy <cat.hoanghuy@gmail.com>
2012-11-13 22:35:38 +0700

    add more constance

--- DevFortress/src/devfortress/utilities/Constant.java
+++ DevFortress/src/devfortress/utilities/Constant.java
@@ -13,4 +13,6 @@ public class Constant {
     public static int MAX_FUCNTION_POINT_EASY = 100;
     public static int MAX_FUCNTION_POINT_MEDIUM = 200;
     public static int MAX_FUNCTION_POINT_DIFFICULT = 400;
+    public static int PROJECT_MAX = 5;
+    public static int EMPLOYEE_MAX = 10;
 }
