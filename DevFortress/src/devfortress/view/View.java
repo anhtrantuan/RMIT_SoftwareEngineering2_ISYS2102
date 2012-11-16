5e89e61c6645ef06d8c25efcba6cf533f57f2d02
anhtrantuan <anhtrantuan.hcmc@gmail.com>
2012-11-16 15:40:35 +0700

    View - Add action listeners for buttons Controller - Create button listeners for main screen and developers table

--- DevFortress/src/devfortress/view/View.java
+++ DevFortress/src/devfortress/view/View.java
@@ -12,5 +12,9 @@ import java.awt.event.ActionListener;
  */
 public interface View {
 
+    void addDevelopersTableButtonListener(ActionListener developersTableButtonListener);
+
+    void setUpDisplay();
+
     void addButtonListener(ActionListener buttonListener);
 }
--- DevFortress/src/devfortress/view/editors/DevelopersTableButtonCellEditor.java
+++ DevFortress/src/devfortress/view/editors/DevelopersTableButtonCellEditor.java
@@ -5,7 +5,6 @@
 package devfortress.view.editors;
 
 import java.awt.Component;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.DefaultCellEditor;
 import javax.swing.JButton;
@@ -18,7 +17,7 @@ import javax.swing.JTable;
  */
 public class DevelopersTableButtonCellEditor extends DefaultCellEditor {
 
-    /* Declare button text. */
+    private ActionListener buttonListener;
     private String buttonText;
 
     /**
@@ -26,8 +25,10 @@ public class DevelopersTableButtonCellEditor extends DefaultCellEditor {
      *
      * @param checkBox
      */
-    public DevelopersTableButtonCellEditor(JCheckBox checkBox) {
+    public DevelopersTableButtonCellEditor(JCheckBox checkBox,
+            ActionListener buttonListener) {
         super(checkBox);
+        this.buttonListener = buttonListener;
     }
 
     /**
@@ -46,13 +47,11 @@ public class DevelopersTableButtonCellEditor extends DefaultCellEditor {
             int row, int column) {
         /* Get button component and set action listener. */
         JButton button = (JButton) value;
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                throw new UnsupportedOperationException("Not supported yet.");
-            }
-        });
+        button.addActionListener(buttonListener);
         buttonText = button.getText();
+
+        /* Set employee's name as button's action command. */
+        button.setActionCommand((String) table.getValueAt(row, 0));
 
         /* Return button component. */
         return button;
