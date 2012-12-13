dd7c7ff9ef06a310fdd458980787a1b0e1447f9c
anhtrantuan <anhtrantuan.hcmc@gmail.com>
2012-12-13 21:54:37 +0700

    Update Look and Feel

--- DevFortress/src/devfortress/DevFortress.java
+++ DevFortress/src/devfortress/DevFortress.java
@@ -8,64 +8,30 @@ import devfortress.controller.Controller;
 import devfortress.model.Beer;
 import devfortress.model.Computer;
 import devfortress.model.Food;
-import devfortress.model.project.Project;
 import devfortress.model.dificulity.EasyLevel;
 import devfortress.model.employee.Employee;
 import devfortress.model.exception.EmployeeIsBusyException;
 import devfortress.model.exception.OvercrowdedException;
 import devfortress.model.facade.Engine;
+import devfortress.model.project.Project;
 import devfortress.utilities.Utilities;
 import devfortress.view.DevFortressView;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  *
  * @author tommy
  */
 public class DevFortress {
-    
+
     /**
      * DevFortress's driver function.
      *
      * @param args
      */
     public static void main(String[] args) {
-        /*
-         * Set the Nimbus look and feel
-         */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /*
-         * If Nimbus (introduced in Java SE 6) is not available, stay with the
-         * default look and feel. For details see
-         * http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-         */
-        try {
-            if (javax.swing.UIManager.getSystemLookAndFeelClassName().equals(javax.swing.UIManager.getCrossPlatformLookAndFeelClassName())) {
-                for (javax.swing.UIManager.LookAndFeelInfo info :
-                        javax.swing.UIManager.getInstalledLookAndFeels()) {
-                    if ("Nimbus".equals(info.getName())) {
-                        javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                        break;
-                    }
-                }
-            } else {
-                javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
-            }
-
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(DevFortress.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            Logger.getLogger(DevFortress.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            Logger.getLogger(DevFortress.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (UnsupportedLookAndFeelException ex) {
-            Logger.getLogger(DevFortress.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
         /*
          * Create and display the form
          */
