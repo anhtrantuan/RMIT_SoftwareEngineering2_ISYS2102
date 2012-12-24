d640bf67a19f158b9e07d6a1223456b493993857
Dang Quang Binh <magician0811@gmail.com>
2012-12-25 04:42:31 +0700

    update

--- DevFortress/src/devfortress/view/animation/eventAnimation/DeveloperIsSickEventAnimation.java
+++ DevFortress/src/devfortress/view/animation/eventAnimation/DeveloperIsSickEventAnimation.java
@@ -6,10 +6,15 @@ package devfortress.view.animation.eventAnimation;
 
 import com.tabuto.j2dgf.Game2D;
 import com.tabuto.j2dgf.Group;
+import devfortress.view.animation.GameAnimationEngine;
 import devfortress.view.animation.GameSprite;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 
 /**
  *
@@ -36,17 +41,25 @@ public class DeveloperIsSickEventAnimation extends Game2D {
 
     @Override
     public void initGame() {
-        /* Get resource URLs. */
-        URL floorsURL = getClass().getResource("../resources/imgFloors.png"),
-                bedURL = getClass().getResource("../resources/sickBed.png"),
-                devURL = getClass().getResource("../resources/sick.png"),
-                doctorURL = getClass().getResource("../resources/doctor_moving.png"),
-                doctorTalkingURL = getClass().getResource("../resources/doctor_talking.png");
-        /* Load resource into place. */
-        FLOORS_IMAGE = ImageIO.read(floorsURL);
-        BED_IMAGE = ImageIO.read(bedURL);
-        DEV_IMAGE = ImageIO.read(devURL);
-        DOCTOR_IMAGE = ImageIO.read(doctorURL);
-        DOCTOR_TALKING_IMAGE = ImageIO.read(doctorTalkingURL);
+        try {
+            /*
+             * Get resource URLs.
+             */
+            URL floorsURL = getClass().getResource("../resources/imgFloors.png"),
+                    bedURL = getClass().getResource("../resources/sickbed.png"),
+                    devURL = getClass().getResource("../resources/sick.png"),
+                    doctorURL = getClass().getResource("../resources/doctor_moving.png"),
+                    doctorTalkingURL = getClass().getResource("../resources/doctor_talking.png");
+            /*
+             * Load resource into place.
+             */
+            FLOORS_IMAGE = ImageIO.read(floorsURL);
+            BED_IMAGE = ImageIO.read(bedURL);
+            DEV_IMAGE = ImageIO.read(devURL);
+            DOCTOR_IMAGE = ImageIO.read(doctorURL);
+            DOCTOR_TALKING_IMAGE = ImageIO.read(doctorTalkingURL);
+        } catch (Exception ex) {
+            Logger.getLogger(GameAnimationEngine.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
