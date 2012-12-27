1028ff43dbe1643f34f56f0f5633baf0f1f50de8
anthrantuan <anhtrantuan.hcmc@gmail.com>
2012-12-27 22:22:21 +0700

    Update

--- DevFortress/test/devfortress/model/event/ProjectEventTest.java
+++ DevFortress/test/devfortress/model/event/ProjectEventTest.java
@@ -8,38 +8,29 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
  * @author Cat Hoang Huy
  */
 public class ProjectEventTest {
-    
+
     public ProjectEventTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
-    @Test
-    public void
 }
