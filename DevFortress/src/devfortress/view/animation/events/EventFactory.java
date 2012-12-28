1d568932812739035a104e9838b7d4405be4cc8a
Dang Quang Binh <magician0811@gmail.com>
2012-12-28 21:50:13 +0700

    add event burning

--- DevFortress/src/devfortress/view/animation/events/EventFactory.java
+++ DevFortress/src/devfortress/view/animation/events/EventFactory.java
@@ -58,6 +58,6 @@ public class EventFactory {
             return new GoldenEmployee(dimension);
         }
 
-        return new WarErupts(dimension);
+        return new CompanyIsBurnt(dimension);
     }
 }
